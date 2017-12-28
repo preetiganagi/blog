721204e6920490b8d70a9dbc0cfd656922b6e70e
Preeti Ganagi <you@example.com>
2017-12-28 16:47:34 +0530

    controller,images,css,header and footer are added

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>footer </title>
+	<link href="css/layout4.css" rel="stylesheet">
+	<link href="css/layout3.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="blog/public/css/footer.css">
+</head>
+<body>
+	<div class="container">
+
+    <hr>
+        <div class="text-center center-block">
+            <p class="txt-railway">- MyApplication.com -</p><br />
+              <div class="col-lg-3 col-md-6">
+                <h3>Contact:</h3>
+				<p>Have a question or feedback? Contact me!</p>
+				<p><a href="" title="Contact me!"><i class="fa fa-envelope"></i> Contact</a></p>
+			</div>
+
+            <h3>Follow:</h3>
+                <a href="https://www.facebook.com"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+	        
+</div>
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header</title>
+	<link rel="stylesheet" href="css/layout1.css">
+	
+	<link href="css/layout2.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">My Application</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Notes</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+</body>
+</html>
